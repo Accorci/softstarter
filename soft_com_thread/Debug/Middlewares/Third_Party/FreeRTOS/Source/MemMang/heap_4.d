ca9c051e8de71baf46a0006c13d56dace4302259
Accorci <pedrooacorsi@gmail.com>
2018-11-04 14:27:00 -0200

    Create heap_4.d

---
+++ soft_com_thread/Debug/Middlewares/Third_Party/FreeRTOS/Source/MemMang/heap_4.d
@@ -0,0 +1,32 @@
+Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Inc/FreeRTOSConfig.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Inc/main.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Inc/FreeRTOSConfig.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Inc/main.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/Pedro/Desktop/soft_com_thread/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
